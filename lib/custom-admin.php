f3d57bbfe9deaad85f01403c399431370989e250
Albin Metthey <a.metthey@gmail.com>
2020-12-08 13:01:50 +0100

    add custom admin for editor

--- lib/custom-admin.php
+++ lib/custom-admin.php
@@ -6,18 +6,42 @@
  */
 function _themename_remove_options_menu(){
 
-  // remove_menu_page( 'edit.php' );                   //Posts
-  // remove_menu_page( 'edit-comments.php' );          //Comments
-  // remove_menu_page( 'index.php' );                  //Dashboard
-  // remove_menu_page( 'jetpack' );                    //Jetpack*
-  // remove_menu_page( 'upload.php' );                 //Media
-  // remove_menu_page( 'edit.php?post_type=page' );    //Pages
-  // remove_menu_page( 'themes.php' );                 //Appearance
-  // remove_menu_page( 'plugins.php' );                //Plugins
-  // remove_menu_page( 'users.php' );                  //Users
-  // remove_menu_page( 'tools.php' );                  //Tools
-  // remove_menu_page( 'options-general.php' );        //Settings
+    // remove_menu_page( 'edit.php' );                   //Posts
+    // remove_menu_page( 'edit-comments.php' );          //Comments
+    // remove_menu_page( 'index.php' );                  //Dashboard
+    // remove_menu_page( 'jetpack' );                    //Jetpack*
+    // remove_menu_page( 'upload.php' );                 //Media
+    // remove_menu_page( 'edit.php?post_type=page' );    //Pages
+    // remove_menu_page( 'themes.php' );                 //Appearance
+    // remove_menu_page( 'plugins.php' );                //Plugins
+    // remove_menu_page( 'users.php' );                  //Users
+    // remove_menu_page( 'tools.php' );                  //Tools
+    // Vemove_menu_page( 'options-general.php' );        //Settings
 
 }
 add_action( 'admin_menu', '_themename_remove_options_menu' );
+
+// remove when user is editor
+function remove_menus(){
+    // get current login user's role
+    $roles = wp_get_current_user()->roles;
+
+    // test role
+    if( !in_array('editor',$roles)){
+        return;
+    }
+
+    //remove menu from site backend.
+    remove_menu_page( 'index.php' ); //Dashboard
+    remove_menu_page( 'edit.php' ); //Posts
+    remove_menu_page( 'upload.php' ); //Media
+    remove_menu_page( 'edit-comments.php' ); //Comments
+    remove_menu_page( 'themes.php' ); //Appearance
+    remove_menu_page( 'plugins.php' ); //Plugins
+    remove_menu_page( 'users.php' ); //Users
+    remove_menu_page( 'tools.php' ); //Tools
+    remove_menu_page( 'options-general.php' ); //Settings
+    // remove_menu_page( 'edit.php?post_type=page' ); //Pages
+}
+add_action( 'admin_menu', 'remove_menus' , 100 );
 ?>
